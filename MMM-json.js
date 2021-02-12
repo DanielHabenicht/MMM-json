414818f599080d8654afe38a24bf5140a2f5f802
Restyled.io <commits@restyled.io>
2021-02-12 10:19:37 +0000

    Restyled by clang-format

--- MMM-json.js
+++ MMM-json.js
@@ -6,46 +6,43 @@
 
 Module.register("MMM-json", {
   // Default module config.
-  defaults: {
-    url: "https://jsonplaceholder.typicode.com/users",
-    refreshInterval: 1000 * 60 * 5 //5 minutes
+  defaults : {
+    url : "https://jsonplaceholder.typicode.com/users",
+    refreshInterval : 1000 * 60 * 5 // 5 minutes
   },
 
-  start: function () {
+  start : function() {
     Log.info("Starting module: " + this.name);
     this.loaded = false;
     this.getData();
 
     var self = this;
-    //Schedule updates
-    setInterval(function () {
+    // Schedule updates
+    setInterval(function() {
       self.getData();
       self.updateDom();
     }, this.config.refreshInterval);
   },
 
-  //Import additional CSS Styles
-  getStyles: function () {
-    return ["mmm-json.css"];
-  },
+  // Import additional CSS Styles
+  getStyles : function() { return [ "mmm-json.css" ]; },
 
-  //Contact node helper for data
-  getData: function () {
+  // Contact node helper for data
+  getData : function() {
     Log.info("MMM-json: getting data");
 
-    this.sendSocketNotification("MMM_JSON_GET_REQUEST", {
-      config: this.config,
-      identifier: this.identifier
-    });
+    this.sendSocketNotification(
+        "MMM_JSON_GET_REQUEST",
+        {config : this.config, identifier : this.identifier});
   },
 
-  //Handle node helper response
-  socketNotificationReceived: function (notification, payload) {
-    if (notification === "MMM_JSON_GET_RESPONSE" && payload.identifier == this.identifier) {
+  // Handle node helper response
+  socketNotificationReceived : function(notification, payload) {
+    if (notification === "MMM_JSON_GET_RESPONSE" &&
+        payload.identifier == this.identifier) {
       if (payload.error === true) {
         console.error(
-          "MMM-JSON: An Error occured while fetching your response. Please have a look at the server log."
-        );
+            "MMM-JSON: An Error occured while fetching your response. Please have a look at the server log.");
         this.loaded = false;
       } else {
         this.loaded = true;
@@ -55,25 +52,26 @@ Module.register("MMM-json", {
     }
   },
   // Override the Header generator
-  getHeader: function () {
+  getHeader : function() {
     // If an Icon should be displayed we need our own header
     if (this.config.headerIcon) {
       return "";
     } else {
-      // TODO: Remove `this.config.header` in a subsequent version (major) as it was wrongly implemented in the config on first release #
+      // TODO: Remove `this.config.header` in a subsequent version (major) as it
+      // was wrongly implemented in the config on first release #
       return this.data.header ? this.data.header : this.config.header;
     }
   },
 
   // Override dom generator.
-  getDom: function () {
+  getDom : function() {
     var wrapper = document.createElement("div");
     if (this.config.url === "") {
       wrapper.innerHTML = "Missing configuration.";
       return wrapper;
     }
 
-    //Display loading while waiting for API response
+    // Display loading while waiting for API response
     if (!this.loaded) {
       wrapper.innerHTML = "Loading...";
       return wrapper;
@@ -86,9 +84,8 @@ Module.register("MMM-json", {
       var imgDiv = document.createElement("div");
 
       var sTitle = document.createElement("p");
-      sTitle.innerHTML = this.data.header
-        ? this.data.header
-        : this.config.header;
+      sTitle.innerHTML =
+          this.data.header ? this.data.header : this.config.header;
       sTitle.className += "normal";
 
       var icon = document.createElement("i");
@@ -111,11 +108,9 @@ Module.register("MMM-json", {
 
       titleTr.innerHTML = this.response[i].title + ":";
       dataTr.innerHTML =
-        (this.response[i].prefix ? this.response[i].prefix : "") +
-        " " +
-        this.response[i].value +
-        " " +
-        (this.response[i].suffix ? this.response[i].suffix : "");
+          (this.response[i].prefix ? this.response[i].prefix : "") + " " +
+          this.response[i].value + " " +
+          (this.response[i].suffix ? this.response[i].suffix : "");
 
       titleTr.className += " small regular bright";
       dataTr.className += " small light bright";
