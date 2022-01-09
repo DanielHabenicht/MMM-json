392350d9bde8081c702253f3d94990361c0ec0ea
Daniel Habenicht <daniel-habenicht@outlook.de>
2022-01-09 14:09:09 +0100

    fix: use node-fetch instead of request
    fixes `request` is deprecated #7
    
    BREAKING CHANGE: use `fetchOptions` instead of `request` in you config

--- node_helper.js
+++ node_helper.js
@@ -1,4 +1,5 @@
-var request = require("request");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 var jp = require("jsonpath");
 const { jq } = require("jq.node");
 var NodeHelper = require("node_helper");
@@ -42,62 +43,55 @@ module.exports = NodeHelper.create({
     console.log("Notification: " + notification + " Payload:", payload);
 
     if (notification === "MMM_JSON_REQUEST") {
-      req_params = {
-        url: payload.config.url,
-        json: true,
-        ...payload.config.request
-      };
-      console.debug(self.name + " req_params:", req_params);
-      request(req_params, async function (error, response, jsonData) {
-        if (!error && Math.floor(response.statusCode / 100) === 2) {
-          var responseObject;
+      fetch(payload.config.url, payload.config.fetchOptions)
+        .then(async (response) => {
+          //Success
+          console.debug(response);
+          if (Math.floor(response.status / 100) === 2) {
+            var responseObject;
 
-          jsonData = await do_jq(payload.config.jq, jsonData);
-          if (
-            payload.config.values == undefined ||
-            payload.config.values.length == 0
-          ) {
-            // Values are not defined return whole object properties
-            responseObject = {
-              identifier: payload.identifier,
-              data: Object.keys(jsonData).map((prop) => {
-                return { title: prop, value: jsonData[prop] };
-              })
-            };
-          } else {
-            // Values are defined, get what the user wants
-            responseObject = {
-              identifier: payload.identifier,
-              data: payload.config.values.map((val) => {
-                return {
-                  ...val,
-                  value:
-                    val.numberDevisor != undefined
-                      ? (
-                          jp.query(jsonData, val.query)[0] / val.numberDevisor
-                        ).toFixed(3)
-                      : jp.query(jsonData, val.query)[0]
-                };
-              })
-            };
+            jsonData = await do_jq(payload.config.jq, await response.json());
+            if (
+              payload.config.values == undefined ||
+              payload.config.values.length == 0
+            ) {
+              // Values are not defined return whole object properties
+              responseObject = {
+                identifier: payload.identifier,
+                data: Object.keys(jsonData).map((prop) => {
+                  return { title: prop, value: jsonData[prop] };
+                })
+              };
+            } else {
+              // Values are defined, get what the user wants
+              responseObject = {
+                identifier: payload.identifier,
+                data: payload.config.values.map((val) => {
+                  return {
+                    ...val,
+                    value:
+                      val.numberDevisor != undefined
+                        ? (
+                            jp.query(jsonData, val.query)[0] / val.numberDevisor
+                          ).toFixed(3)
+                        : jp.query(jsonData, val.query)[0]
+                  };
+                })
+              };
+            }
+
+            self.sendSocketNotification("MMM_JSON_RESPONSE", responseObject);
           }
-
-          self.sendSocketNotification("MMM_JSON_RESPONSE", responseObject);
-        } else {
+        })
+        .catch((error) => {
+          console.error(error);
+          // Error
           self.sendSocketNotification("MMM_JSON_RESPONSE", {
             identifier: payload.identifier,
             error: true
           });
-          console.error(
-            self.name + " error:",
-            error,
-            "statusCode:",
-            response && response.statusCode,
-            "statusMessage:",
-            response && response.statusMessage
-          );
-        }
-      });
+          console.error(self.name + " error:", error);
+        });
     }
   }
 });
